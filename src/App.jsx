f64a75de000bae0e3c1acc3315663a058fe51b5c
Brandon Hill <brandonjon26@gmail.com>
2025-08-30 20:49:42 -0500

    Created form for adding a todo

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,17 @@
+import { useState } from "react";
+import { TodoForm } from "./components/TodoForm/TodoForm";
 import styles from "./App.module.css";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+
+  function handleCreate(newTodo) {
+    setTodos((prevTodos) => [
+      ...prevTodos, 
+      { id: `${prevTodos.length + 1}`, ...newTodo }
+    ]);
+  }
+
   return (
     <div className={styles.App}>
       <header className={styles.Header}>
@@ -8,7 +19,10 @@ function App() {
         <h2 className={styles.Title}>To-Do App</h2>
       </header>
 
-      <div className={styles.AppContainer}>Content</div>
+      <div className={styles.AppContainer}>
+        <TodoForm onCreate={handleCreate} />
+        {JSON.stringify(todos)}
+      </div>
     </div>
   );
 }
