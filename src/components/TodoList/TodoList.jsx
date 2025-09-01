17ec879d4e3fb88702d0448ffd47b1a97e8d2be6
Brandon Hill <brandonjon26@gmail.com>
2025-08-31 21:48:55 -0500

    Delete items from list; display default message when list is empty

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,14 +1,22 @@
 import { TodoListItem } from "../TodoListItem/TodoListItem";
 import styles from "./TodoList.module.css";
 
-export function TodoList({ todos, onUpdate }) {
+export function TodoList({ todos, onUpdate, onDelete }) {
   return (
     <section>
       <h3>To-Do's</h3>
+      {!todos.length && (
+        <p>There are no items on your todo list currently. Good job!</p>
+      )}
 
       <ul className={styles.TodoList}>
         {todos.map((todo) => (
-          <TodoListItem key={todo.id} todo={todo} onUpdate={onUpdate} />
+          <TodoListItem
+            key={todo.id}
+            todo={todo}
+            onUpdate={onUpdate}
+            onDelete={onDelete}
+          />
         ))}
       </ul>
     </section>
