4b78209eedf8034710e14e7010ea3d652befdf23
Brandon Hill <brandonjon26@gmail.com>
2025-09-07 19:09:25 -0500

    Added deadline field validation

--- src/schemas/todo.js
+++ src/schemas/todo.js
@@ -1,7 +1,15 @@
 import * as Yup from "yup";
 import { PRIORITIES } from "../constants/priorities";
 
-export function getTodoSchema() {
+export function getTodoSchema({ isNew = false } = {}) {
+  const deadlineRule = Yup.string()
+    .nullable()
+    .transform((value, originValue) => (originValue === "" ? null : value))
+    .matches(
+      /^\d{4}-\d{2}-\d{2}$/,
+      "Please enter a valid date using the YYYY-MM-DD format"
+    );
+
   return Yup.object().shape({
     name: Yup.string()
       .required("Name is required")
@@ -11,7 +19,16 @@ export function getTodoSchema() {
       200,
       "Description should have max length of 200 characters"
     ),
-    deadline: Yup.string(),
+    deadline: isNew
+      ? deadlineRule.test(
+          "is-future-date",
+          "Deadline can't be in the past",
+          (value) => {
+            const today = new Date().toISOString().split("T")[0];
+            return value ? value >= today : true;
+          }
+        )
+      : deadlineRule,
     priority: Yup.string()
       .required("Priority is not valid value")
       .oneOf(Object.keys(PRIORITIES), "Priority is not valid value"),
