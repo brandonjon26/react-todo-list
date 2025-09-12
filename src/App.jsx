2869bb535aaae7976160468bf2ccd0c61b309cb1
Brandon Hill <brandonjon26@gmail.com>
2025-09-11 19:56:21 -0500

    Set up API using mockAPI tool; moved data to mockAPI tool; added get request to fetch data

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,25 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { TodoForm } from "./components/TodoForm/TodoForm";
 import { TodoList } from "./components/TodoList/TodoList";
 import { TodoFilters } from "./components/TodoFilters/TodoFilters";
 import styles from "./App.module.css";
 
-const TODOS_DEFAULT = [
-  {
-    id: "1",
-    name: "Buy an Ice Cream",
-    description: "The white one with chocolate",
-    deadline: "2025-08-30",
-    priority: "low",
-    completed: false,
-  },
-  {
-    id: "2",
-    name: "Sell old MacBook Pro 2018",
-    description: "Try to sell it on eBay",
-    deadline: "2025-09-05",
-    priority: "high",
-    completed: false,
-  },
-  {
-    id: "3",
-    name: "Charge Powerbank",
-    description: "For my upcoming vacation",
-    deadline: "2025-08-31",
-    priority: "medium",
-    completed: true,
-  },
-  {
-    id: "4",
-    name: "Test Todo only with a name",
-    description: "",
-    deadline: "",
-    priority: "none",
-    completed: false,
-  },
-];
-
 function App() {
-  const [todos, setTodos] = useState(TODOS_DEFAULT);
+  const [todos, setTodos] = useState([]);
   const [filters, setFilters] = useState({});
+
+  function fetchTodos() {
+    fetch(`${import.meta.env.VITE_MOCKAPI_BASE_URL}/todos`, {
+      method: "GET",
+      headers: { "content-type": "application/json" },
+    })
+      .then((response) => !!response.ok && response.json())
+      .then(setTodos);
+  }
+
+  useEffect(() => {
+    fetchTodos();
+  }, []);
 
   function handleCreate(newTodo) {
     setTodos((prevTodos) => [
