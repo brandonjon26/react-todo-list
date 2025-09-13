0c2fefc8f3254768a51cd7eba83d30a70a0cf464
Brandon Hill <brandonjon26@gmail.com>
2025-09-13 11:44:47 -0500

    Moved API Calls to separate file

---
+++ src/api.js
@@ -0,0 +1,39 @@
+const API_BASE_URL = import.meta.env.VITE_MOCKAPI_BASE_URL;
+
+export const api = {
+  todos: {
+    getAll(params = {}) {
+      const searchParams = new URLSearchParams(params).toString();
+
+      return fetch(`${API_BASE_URL}todos?${searchParams}`, {
+        method: "GET",
+        headers: { "content-type": "application/json" },
+      }).then((response) => {
+        if (response.ok) return response.json();
+        if (response.status === 404) return [];
+      });
+    },
+
+    create(data) {
+      return fetch(`${API_BASE_URL}todos`, {
+        method: "POST",
+        headers: { "content-type": "application/json" },
+        body: JSON.stringify(data),
+      }).then((response) => !!response.ok && response.json());
+    },
+
+    update(id, data) {
+      return fetch(`${API_BASE_URL}todos/${id}`, {
+        method: "PUT",
+        headers: { "content-type": "application/json" },
+        body: JSON.stringify(data),
+      }).then((response) => !!response.ok && response.json());
+    },
+
+    delete(id) {
+      return fetch(`${API_BASE_URL}todos/${id}`, {
+        method: "DELETE",
+      }).then((response) => !!response.ok && response.json());
+    },
+  },
+};
